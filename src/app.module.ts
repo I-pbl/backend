435e5c93d35c3008368e5ec4ff51555665f25255
SeungWon <iwon@kookmin.ac.kr>
2024-04-08 21:51:52 -0700

    test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AuthController } from './auth/auth.controller';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { getTypeOrmConfig } from './configs/typeorm.config';
-import { BoardService } from './board/board.service';
-import { BoardController } from './board/board.controller';
 import { BoardModule } from './board/board.module';
 
 @Module({
